822e391477f695a1f8de115f46d607b3ea1260f5
sahil <sahiillx02@gmail.com>
2024-05-08 09:24:20 +0530

    Input name servlet

---
+++ demo/src/main/java/com/projectdemo/controller/InputNameServlet.java
@@ -0,0 +1,32 @@
+package com.projectdemo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/nameservlet")
+public class InputNameServlet extends HttpServlet {
+
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String name = request.getParameter("name");
+		name = name.toUpperCase();
+
+		System.out.println("Name to upper case : " + name);
+
+		// PRINT TO BROWSER
+
+		PrintWriter display = response.getWriter();
+		display.println(name);
+
+		// try {
+		// PrintWriter out = response.getWriter();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+
+	}
+}
