512651a130aed4a65e7af755595b2f22e281b7a0
Professor <sahiillx02@gmail.com>
2024-06-13 17:02:31 +0530

    filter

--- filter/src/main/java/com/project/controller/OddEvenServlet.java
+++ filter/src/main/java/com/project/controller/OddEvenServlet.java
@@ -1,36 +1,14 @@
 package com.project.controller;
 
-import java.io.IOException;
-
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/OddEvenServlet")
-public class OddEvenServlet extends HttpServlet {
-
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		System.out.println("OddEven servlet.");
-
-		String numStr = request.getParameter("num");
-
-		int num = Integer.parseInt(numStr);
-
-		if (num % 2 == 0) {
-			request.setAttribute("msg", "EVEN");
-		} else {
-			request.setAttribute("msg", "ODD");
-		}
-
-		request.setAttribute("num", num);
-
-		RequestDispatcher rd = request.getRequestDispatcher("OddEvenOutput.jsp");
-		rd.forward(request, response);
-
+public class OddEvenServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		
 	}
 }
--- filter/src/main/webapp/OddEvenInput.jsp
+++ filter/src/main/webapp/OddEvenInput.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,12 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>Odd Even Input</h1>
 
-	<form action="OddEvenServlet" method="post">
-		Number : <input type="text" name="num" />
-		${numError} <br>
-		<br> <input type="submit" value="Process" />
-	</form>
 </body>
 </html>
